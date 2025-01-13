e228324b1ff6d7d7f6b06b4192b44e92eb0441d5
Anastasiia Medved <nastya0662793393@gmail.com>
2025-01-13 12:26:48 +0100

    second part

---
+++ js/01-array.js
@@ -0,0 +1,32 @@
+const friends = [
+  { name: 'Mango', online: false },
+  { name: 'Kiwi', online: true },
+  { name: 'Poly', online: true },
+  { name: 'Ajax', online: false },
+];
+
+console.log(friends);
+
+// const findFriendByName = function (allFriends, friendName) {
+//   for (const friend of allFriends) {
+//     if (friend.name.toLowerCase() === friendName.toLowerCase()) {
+//       return friend;
+//     }
+//   }
+//   return `${friendName} не знайдено!`;
+// };
+
+// console.log(findFriendByName(friends, 'Poly'));
+// console.log(findFriendByName(friends, 'Chelsy'));
+//* -----------------------------------------------------------------------------
+// const getAllNames = function (allFriends) {
+//   const allNames = [];
+
+//   for (const friend of allFriends) {
+//     allNames.push(friend.name);
+//   }
+//   return allNames;
+// };
+
+// console.log(getAllNames(friends));
+//* -----------------------------------------------------------------------------
